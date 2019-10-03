445622d7375a81f9499528233882c9839bae086b
Vítor Norberto <vithor.norberto@gmail.com>
2019-10-03 10:30:34 -0300

    Remove VmWare, STS e opera, adiciona docker-desktop e chrome

--- Script-chocolatey.bat
+++ Script-chocolatey.bat
@@ -3,53 +3,33 @@ echo Iniciando script
 echo Instalando gerenciador de pacotes chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-echo Iniciando instalacao: Opera navegador
-choco install opera -y
+choco install googlechrome -y
 
-echo Iniciando instalacao: Visual Studio Code
 choco install vscode -y
 
-echo Iniciando instalacao: JDK8
 choco install jdk8 -y
 
-echo Iniciando instalacao: NodeJS LTS-10.16
 choco install nodejs-lts -y
 
-echo Iniciando instalacao: Git 
 choco install git -y
 
-echo Iniciando instalacao: Python3
 choco install python -y
 
-echo Iniciando instalacao: IDE Intellij-Ultimate
 choco install intellijidea-ultimate -y
 
-echo Iniciando instalacao: IDE Pycharm-Ultimate
 choco install pycharm -y
 
-echo Iniciando instalacao: Datagrip
 choco install datagrip -y
 
-echo Iniciando instalacao: jhipster versão 4.14.5
-choco install jhipster --version 4.14.5 -y
-
-echo Iniciando instalacao: mpc-hc
 choco install mpc-hc  -y
 
-echo Iniciando instalacao: Postman
 choco install postman -y
 
-echo Iniciando instalacao: Arduino IDE 1.8.9
 choco install arduino -y
 
-echo Iniciando instalacao: 7zip
 choco install 7zip -y
 
-echo Iniciando instalacao: VMware workstation player
-choco install vmware-workstation-player -y
-
-echo Iniciando instalacao: springtoolsuite
-choco install springtoolsuite -y
+choco install docker-desktop -y
 
 echo Finalizando Script
 
