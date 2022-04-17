9192c50b9c537ca1053009caf5112dd774c4cc09
Vítor de Santana O Norberto <39844018+Norbertoooo@users.noreply.github.com>
2022-04-17 12:11:55 -0300

    Update Script-chocolatey.bat

--- Script-chocolatey.bat
+++ Script-chocolatey.bat
@@ -3,27 +3,55 @@ echo Iniciando script
 echo Instalando gerenciador de pacotes chocolatey
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
+echo Iniciando instalação - googlechrome
+
 choco install googlechrome -y
+
+echo Iniciando instalação - vscode
 
 choco install vscode -y
 
-choco install openjdk11 -y
+echo Iniciando instalação - jdk 17 - liberica
+
+choco install liberica17jdk -y
+
+echo Iniciando instalação - nodejs - lts
 
 choco install nodejs-lts -y
 
+echo Iniciando instalação - dbeaver
+
+choco install dbeaver -y
+
+echo Iniciando instalação - git
+
 choco install git -y
+
+echo Iniciando instalação - python
 
 choco install python -y
 
+echo Iniciando instalação - jetbrainstoolbox
+
 choco install jetbrainstoolbox
+
+echo Iniciando instalação - mpc-hc
 
 choco install mpc-hc  -y
 
+echo Iniciando instalação - postman
+
 choco install postman -y
+
+echo Iniciando instalação - arduino
 
 choco install arduino -y
 
+echo Iniciando instalação - 7zip
+
 choco install 7zip -y
+
+echo Iniciando instalação - docker-desktop
 
 choco install docker-desktop -y
 
